e2b54322c6df1bc9a8f0ffd667b473b7098deeec
jseok0917 <139304962+jseok0917@users.noreply.github.com>
2023-08-10 10:48:36 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 348 ms, Memory: 111,460 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,67 +1,116 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
-	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+//		Map<Integer, Integer> map = new HashMap();
+//		// 맵 테스트
+//		for (int i = 0; i < 100; i++) {
+//			map.put(i, i); 
+//		}
+//		
+//		map.replace(3, 7); // 밸류값 바꿔치기가 잘 되는가
+//		System.out.println(map.get(3)); // 키밸류가 잘 들어가는가
 		
-		for (int t = 0; t < 10; t++) {
 		
-			int T =  sc.nextInt();
-			sc.nextLine();
+		//index문제를 생각하지 않아도 되게
+		//애초부터 가로로 확장된 판으로 생각을 한다
+		//행 pointer를 써서 순차적으로 바꿔치기 되는 부분을 기록한다.
+		//bijection - cycle 
+		
+		for (int t = 1; t <= 10; t++) {
+		
+			int T = sc.nextInt();
+			sc.nextLine();	
+			List<Integer> cycle = new ArrayList<>();
 			
-			//인덱스 문제 안생기게 좌우로 한칸 씩 늘려놓는다.
+			
+			//입력받을 판
 			int[][] ladder = new int[100][102];
 			
-			//입력 받기
+			//입력
 			for (int i = 0; i < 100; i++) {
-				for(int j = 1; j < 101; j++) {
+				for (int j = 1; j < 101; j++) {
 					ladder[i][j] = sc.nextInt();
 				}
 			}
 			
-			//도착 지점 기록
+			//도착위치 기록
 			int arrival = 0;
-			
 			for (int j = 1; j < 101; j++) {
-				if (ladder[99][j] == 2){
+				if (ladder[99][j] == 2) {
 					arrival = j;
+					break;
 				}
 			}
 			
-			//도착지점에서 출발하여 거꾸로 진행하는 것으로 생각
+			//이제 행포인터로 돌면서 사다리가 그어진 시점을 모두 기록한다.
+			//도착위치에서 거꾸로 찾아나간다?
 			
-			//임의의 점이 주어졌을 때
-			//그 점은 상, 좌, 우를 탐색하여 좌나 우가 있을경우 그쪽방향으로 진행하고
-			//좌우가 없으면 위로만 가야한다
-			//행 index가 0이 되면 반복문을 멈춰야한다.
-			
-			int n = arrival;
-			int m = 99;
-			
-			while (m > 0) {
-				
-				if (ladder[m][n-1] == 1) {
-					while (ladder[m][n-1] == 1) {
-						n--;
+			for (int i = 99; i >= 0; i--) {
+				for (int j = 1; j < 101; j++) {
+					
+					//시작지점 기록
+					if (ladder[i][j-1] == 0 && ladder[i][j] == 1 && ladder[i][j+1] == 1) {
+						cycle.add(j);
+						
+					//요건 끝지점 기록
+					} else if (ladder[i][j-1] == 1 && ladder[i][j] == 1 && ladder[i][j+1] == 0) {
+						cycle.add(j);
 					}
-				} else if (ladder[m][n+1] == 1) {	
-					while (ladder[m][n+1] == 1) {
-						n++;
+					//시작이 있으면 끝이 있을 것이므로
+					//cycle의 size는 반드시 짝수가 된다.
+	
+				}
+			}
+			
+			//처음에 사다리에 아무것도 그려져 있지 않을때는
+			//시작위치와 도착위치가 모두 같으므로
+			Map<Integer, Integer> map = new HashMap();
+			
+			for (int i = 1; i <= 100; i++) {
+				map.put(i, i);
+			}
+			
+			//N은 반드시 짝수이다
+			int N = cycle.size();
+			//이제 Map을 이용해 도착위치 변화를 기록한다.
+			for (int i = 0; i < N; i += 2) {
+				
+				int temp1 = 0;
+				int temp2 = 0;
+				for (int j = 1; j <= 100; j++) {
+					if (map.get(j) == cycle.get(i)) {
+						temp1 = j;
 					}
 				}
 				
-				m--;
+				for (int j = 1; j <= 100; j++) {
+					if (map.get(j) == cycle.get(i+1)) {
+						temp2 = j;
+					}
+				}
+				
+				int temp = cycle.get(i);
+				map.replace(temp1, cycle.get(i+1));
+				map.replace(temp2, temp);
+				
+		
 			}
 			
-			System.out.printf("#%d %d", t+1, n-1);
-			System.out.println();
-		
-		
+	//		System.out.println(arrival);
+	//		System.out.println(cycle);
+			
+			System.out.printf("#%d %d\n", t,map.get(arrival)-1);
 		
 		}
 		
+	
 		
 	}
 
