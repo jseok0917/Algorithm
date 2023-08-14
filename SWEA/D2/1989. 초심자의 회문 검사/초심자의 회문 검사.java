0765ae5f7c5265504ba0bfccfa2b29d9f5555892
jseok0917 <139304962+jseok0917@users.noreply.github.com>
2023-08-14 17:50:26 +0900

    [D2] Title: 초심자의 회문 검사, Time: 132 ms, Memory: 20,476 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int t = 1; t <= T; t++) {
+			String input = sc.next();
+			int L = input.length();
+			
+			for (int i = 0; i <= (L-1)/2; i++) {
+				
+				if (input.charAt(i) != input.charAt(L-1-i)) {
+					System.out.printf("#%d %d\n", t, 0);
+					break;
+				} 
+				
+				if (i == (L-1)/2) {
+					System.out.printf("#%d %d\n", t, 1);
+				}
+				
+				
+			}
+			
+			
+			
+		}
+		
+		
+	}
+
+}
