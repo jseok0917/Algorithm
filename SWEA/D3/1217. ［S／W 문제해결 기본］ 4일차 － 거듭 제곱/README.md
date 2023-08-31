e81119a43478e4a5b780f4f5c35d919336d96bc4
jseok0917 <139304962+jseok0917@users.noreply.github.com>
2023-09-01 03:18:46 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 133 ms, Memory: 21,700 KB -BaekjoonHub

---
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/README.md
@@ -0,0 +1,11 @@
+# [D3] [S/W 문제해결 기본] 4일차 - 거듭 제곱 - 1217 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14dUIaAAUCFAYD) 
+
+### 성능 요약
+
+메모리: 21,700 KB, 시간: 133 ms, 코드길이: 654 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		for (int tc = 1; tc <= 10; tc++) {
+			//테스트번호
+			int t = sc.nextInt();
+			
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			int result = power(N, M);
+			System.out.printf("#%d %d\n", t, result);
+			
+			
+		}
+		
+		
+	}
+	//거듭제곱 재귀함수 구현
+	public static int power(int N, int M) {
+		//기저조건
+		if (M == 1) {
+			return N;
+		}
+		
+		
+		//재귀조건
+		if (M%2 == 0) {
+			int temp = power(N, M/2);
+			return temp*temp;
+		} else {
+			int temp = power(N, (M-1)/2);
+			return temp*temp*N;
+		}
+		
+		
+	}
+	
+
+}
