4f80e26dd09f0b60f27b96bea5de5678fad00adb
jseok0917 <139304962+jseok0917@users.noreply.github.com>
2023-09-18 11:28:59 +0900

    [Silver IV] Title: 국영수, Time: 804 ms, Memory: 88388 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,74 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Student implements Comparable<Student> {
+	String name;
+	int kor;
+	int eng;
+	int math;
+	
+	public Student() {}
+	
+	public Student(String name, int kor, int eng, int math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	
+	@Override
+	public int compareTo(Student o) {
+		if (this.kor == o.kor) {
+			if (this.eng == o.eng) {
+				if (this.math == o.math) {
+					//이름 사전정렬
+					return this.name.compareTo(o.name);
+				}
+				return o.math - this.math;
+			}
+			return this.eng - o.eng;
+		}
+		
+		return o.kor-this.kor;
+	}
+	
+	
+}
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//학생 수
 		int N = Integer.parseInt(br.readLine());
 		
-		//정보 입력
-		//4개의 정보
-		String[][] scoreInfo = new String[N][4];
+		List<Student> students = new ArrayList<>();
 		
 		for (int i = 0; i < N; i++) {
-			scoreInfo[i] = br.readLine().split(" ");
+			String[] input = br.readLine().split(" ");
+			Student st = new Student();
+			st.name = input[0];
+			st.kor = Integer.parseInt(input[1]);
+			st.eng = Integer.parseInt(input[2]);
+			st.math = Integer.parseInt(input[3]);
+			
+			students.add(st);
 		}
 		
-		//Arrays.sort(퀵정렬)를 이차원 배열에도 적용시키기 위해
-		//Arrays.sort와 Collections.sort는 둘다 안정정렬이다
-		Arrays.sort(scoreInfo, new Comparator<String[]>() {
-			@Override
-			//s1이라는 String배열과 s2라는 String배열을 직접적으로 비교하기 위한
-			//class를 작성한다.
-			public int compare(String[] s1, String[] s2) {
-				if (Integer.parseInt(s1[1]) == Integer.parseInt(s2[1])) {
-					if (Integer.parseInt(s1[2]) == Integer.parseInt(s2[2])) {
-						if (Integer.parseInt(s1[3]) == Integer.parseInt(s2[3])) {
-							//s1과 s2의 성적이 완전히 동일하면 
-							//s1과 s2의 이름순으로 배열한다.
-							
-							//기본세팅은 사전순
-							return s1[0].compareTo(s2[0]);
-							
-						//국어점수와 영어점수가 동일하고
-						//수학 성적이 다르면 
-						//수학점수가 감소하는 순서로
-						}
-						
-						return Integer.parseInt(s2[3])-Integer.parseInt(s1[3]);
-						
-					}
-					
-					//기본 순서는 오름차순
-					//-값이면 앞에 있는 s1이 s1 < s2 이므로 순서 유지
-					//+값이면 변경
-					//같으면 변경x(안정정렬)
-					return Integer.parseInt(s1[2])-Integer.parseInt(s2[2]);
-					
-				}
-				return Integer.parseInt(s2[1])-Integer.parseInt(s1[1]);
-				
-				
-				
-				
-			}
-			
-		});
+		//Comparable에 상속받고있거나
+		//Comparator를 직접 생성해야 
+		Collections.sort(students);
 		
 		StringBuilder sb = new StringBuilder();
-		
 		for (int i = 0; i < N; i++) {
-			sb.append(scoreInfo[i][0]+"\n");
+			sb.append(students.get(i).name+"\n");
 		}
 		
 		System.out.println(sb.toString());
-		
-		
 		
 		
 	}
